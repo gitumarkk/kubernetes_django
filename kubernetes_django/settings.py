905f66b18e09613adda0dce0ac91e4f10adf0020
Mark Gituma <gitumarkk@gmail.com>
2018-01-09 18:26:34 -0500

    updated comment

--- kubernetes_django/settings.py
+++ kubernetes_django/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'health_check',                             # required
+    # Running Health Checks
+    'health_check',
     'health_check.db',
 ]
 
