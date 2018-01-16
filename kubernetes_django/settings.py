92bc951752f53e79dabc5ed88a21e7b7e621f9cf
Mark Gituma <gitumarkk@gmail.com>
2018-01-16 17:51:44 -0500

    added redis cache

--- kubernetes_django/settings.py
+++ kubernetes_django/settings.py
@@ -90,6 +90,17 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "example"
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
