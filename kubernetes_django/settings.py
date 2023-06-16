2d8505c5680da4045deab9de35833542fb01884f
Shuhao Lai <shuhaolai18@gmail.com>
2023-06-15 23:36:28 -0400

    fixed bugs

--- kubernetes_django/settings.py
+++ kubernetes_django/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'kubernetes_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
